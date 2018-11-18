3a66830babbbdebe8b2d3799f358e8bbf76269f3
Joaquin <joochour@gmail.com>
2018-11-17 21:47:00 -0300

    Removed unnecessary .jars

--- redisConnection/src/mongoConnection/MongoConnection.java
+++ redisConnection/src/mongoConnection/MongoConnection.java
@@ -19,15 +19,6 @@ public class MongoConnection {
 		DBCollection collection = database.getCollection("usuarios");
 		System.out.println(collection.getCount()); 
 		
-		/*List<Integer> books = Arrays.asList(27464, 747854);
-		DBObject person = new BasicDBObject("_id", "jo")
-                .append("name", "Jo Bloggs")
-                .append("address", new BasicDBObject("street", "123 Fake St")
-                                             .append("city", "Faketon")
-                                             .append("state", "MA")
-                                             .append("zip", 12345))
-                .append("books", books);
-		collection.insert(person);*/
 		DBObject query = new BasicDBObject("nombre", "Juan Perez");
 		DBCursor cursor = collection.find(query);
 		System.out.println(cursor.one());
